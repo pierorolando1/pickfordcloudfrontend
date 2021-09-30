9b52cc8e2c5e88f1b57e3bcec467d3e710e16a62
Piero Rolando <rolandopiero63@gmail.com>
2021-09-30 08:12:45 -0700

    create a file to have the constant word in differents lenguages

---
+++ src/lenguage.ts
@@ -0,0 +1,21 @@
+export const en = {
+    title: "Welcome, what do you want to upload?",
+    image: "An image",
+    video: "An video",
+    pdf: "An PDF",
+    another: "Another file",
+    dragOrClickImg: "Drag the image here (or click here)"
+}
+
+export const es = {
+    title: "Hola, que deseas subir?",
+    image: "Una imagen",
+    video: "Un video",
+    pdf: "Un PDF",
+    another: "Otro archivo",
+    dragOrClickImg: "Arrastra la imagen acá (o clickea esto)"
+}
+
+export default {
+    en, es
+}
